62872b9098cbca8b88cb93419b2dff3e0b6edca6
siuying <siu.ying@gmail.com>
2009-07-09 23:59:08 +0800

    add growl adapter

---
+++ lib/sinatra/pushr/adapters/growl_adapter.rb
@@ -0,0 +1,22 @@
+require 'logger'
+require 'ruby-growl'
+
+module Sinatra
+  module Pushr
+    module Adapters
+      class GrowlAdpater < BaseAdpater
+        def initialize
+          @log = Logger.new($stdout)
+        end
+
+        def send_message(dest, title, message)
+          @log.info("send(#{dest}) -> #{title}: #{message}")
+          g = Growl.new dest, "pushr", ["pushr notification"]
+          g.notify "pushr notification", title, message
+        end
+
+        register self
+      end
+    end
+  end
+end
